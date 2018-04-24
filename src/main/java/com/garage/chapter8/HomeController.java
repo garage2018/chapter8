00be1b48a0bb6ede5f01b37baf575a136995c31a
Configurator Garage <configurator.garage@gmail.com>
2018-04-24 14:22:00 +0600

    adding requestmapping annotation

--- src/main/java/com/garage/chapter8/HomeController.java
+++ src/main/java/com/garage/chapter8/HomeController.java
@@ -2,11 +2,17 @@ package com.garage.chapter8;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+
+    @RequestMapping("/")
     public String goForit() {
         return "view";
     }
+
+
+
 }
